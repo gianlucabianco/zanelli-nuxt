79ad1b9aeb147955279030be3b5f93030723c706
Gianluca Bianco <gianluca.bianco@studiomado.it>
2020-05-09 11:52:05 +0200

    feat: z-services-intro component created

---
+++ components/ZServicesIntro.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="z-hero-services">
+    <div class="z-hero-services__content">
+      <div class="z-hero-services__content__description">
+        <p class="z-hero-services__content__description__item">
+          Sono Francesca Zanelli, psicologa e psicoterapeuta ad orientamento analitico transazionale integrato.
+        </p>
+        <p class="z-hero-services__content__description__item--center">
+          Mi occupo di adulti che stanno attraversando momenti di difficoltà personale e relazionale, tali da comportare sofferenza e di coloro che hanno l'interesse a conoscersi meglio per intraprendere un precorso di consapevolezza e cambiamento.
+        </p>
+        <h2 class="z-hero-services__content__description__item--title">SERVIZI OFFERTI</h2>
+      </div>
+      <div class="z-hero-services__content__img-or-quote">
+        "Quote or image here"
+      </div>
+    </div>
+  </div>
+</template>
+
+<style
+  lang="scss"
+  scoped
+>
+  .z-hero-services {
+
+    width: 100%;
+    height: 100%;
+
+    padding: 44px 0;
+    /* padding: 64px 0; */
+
+    background-color: #D8D8DE;
+    font-size: 22px;
+    line-height: 1.8;
+
+    &__content {
+
+      display: flex;
+
+      width: 100%;
+      height: 100%;
+
+      padding: 0 20px;
+
+      /* background-color: red; */
+
+      @screen lg {
+
+        @apply p-0 w-3/4 mx-auto;
+
+      }
+
+      &__img-or-quote {
+
+        display: none;
+
+        @screen xl {
+
+          display: flex;
+          justify-content: center;
+          align-items: center;
+
+          width: 512px;
+
+          background-color: lightcoral;
+
+        }
+
+      }
+
+      &__description  {
+
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        align-items: flex-start;
+
+        width: 100%;
+        height: 100%;
+
+        @screen xl {
+
+          padding-right: 32px;
+          max-width: 50%;
+
+        }
+
+        &__item {
+
+          &--center {
+
+            max-width: 880px;
+            padding: 32px 0;
+
+          }
+
+          &--title {
+
+            font-size: 26px;
+            font-weight: 800;
+
+          }
+
+        }
+
+      }
+
+    }
+
+  }
+
+</style>
