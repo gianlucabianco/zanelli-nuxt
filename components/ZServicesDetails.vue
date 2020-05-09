bab819b98a33e556e3cbebc77e40013cb1d5f2aa
Gianluca Bianco <gianluca.bianco@studiomado.it>
2020-05-09 15:15:06 +0200

    feat: z-services-details component created

---
+++ components/ZServicesDetails.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="z-services-details">
+    <!-- TODO: / FIXME: v-for + data obj -->
+    <z-accordion
+      title="Consulenza e sostegno psicologico"
+      content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et, architecto aliquam quae quos dicta, culpa dolore provident expedita recusandae quas quibusdam adipisci repudiandae! Molestias recusandae eius itaque vitae quibusdam perspiciatis?"
+    />
+    <z-accordion
+      title="Psicoterapia individuale"
+      content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et, architecto aliquam quae quos dicta, culpa dolore provident expedita recusandae quas quibusdam adipisci repudiandae! Molestias recusandae eius itaque vitae quibusdam perspiciatis?"
+    />
+    <z-accordion
+      title="EMDR"
+      content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et, architecto aliquam quae quos dicta, culpa dolore provident expedita recusandae quas quibusdam adipisci repudiandae! Molestias recusandae eius itaque vitae quibusdam perspiciatis?"
+    />
+    <z-accordion
+      title="Valutazione psicodiagnostica"
+      content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et, architecto aliquam quae quos dicta, culpa dolore provident expedita recusandae quas quibusdam adipisci repudiandae! Molestias recusandae eius itaque vitae quibusdam perspiciatis?"
+    />
+    <z-accordion
+      title="Valutazione DSA in età evolutiva"
+      content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et, architecto aliquam quae quos dicta, culpa dolore provident expedita recusandae quas quibusdam adipisci repudiandae! Molestias recusandae eius itaque vitae quibusdam perspiciatis?"
+    />
+  </div>
+</template>
+
+<script>
+import ZAccordion from '~/components/ZAccordion.vue';
+
+export default {
+  components: {
+    ZAccordion,
+  },
+};
+</script>
+
+<style
+  lang="scss"
+  scoped
+>
+  .z-services-details {
+
+    width: 100%;
+    height: 100%;
+
+    padding: 22px 20px;
+
+    font-size: 22px;
+    line-height: 1.8;
+
+  }
+
+</style>
