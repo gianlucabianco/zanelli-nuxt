e3d01bcc4db13558061524dd68398048eb73712a
Gianluca Bianco <gianluca.bianco@studiomado.it>
2020-05-09 11:50:57 +0200

    feat: z-services-intro included && cleaning

--- pages/index.vue
+++ pages/index.vue
@@ -2,42 +2,21 @@
   <section
     class="z-index"
   >
-    <!-- <nuxt-link to="/posts">
-      <h1>
-        POSTS
-      </h1>
-
-    </nuxt-link> -->
-    <!-- <ul
-      v-for="(post, index) in posts"
-      :key="index"
-    >
-      <li>
-        <nuxt-link to="`/posts/${post.slug}`">
-          POSTS
-        </nuxt-link>
-        {{ post }}
-      </li>
-    </ul> -->
     <z-hero-grid />
+    <z-services-intro />
+    <!-- TODO: services details -->
+    <!-- TODO: posts -->
   </section>
 </template>
 
 <script>
-/* FIXME: NEED THESE ?*/
-import AppLogo from '~/components/AppLogo.vue';
-import ZNavbar from '~/components/ZNavbar.vue';
-import ZMenu from '~/components/ZMenu.vue';
 import ZHeroGrid from '~/components/ZHeroGrid.vue';
-import ZFooter from '~/components/ZFooter.vue';
+import ZServicesIntro from '~/components/ZServicesIntro.vue';
 
 export default {
   components: {
-    AppLogo,
-    ZNavbar,
-    ZMenu,
     ZHeroGrid,
-    ZFooter,
+    ZServicesIntro,
   },
 };
 </script>
